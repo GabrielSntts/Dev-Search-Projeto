c77cc4f1093fa7accb973948dd96bf67e43ca4aa
gabriel <you@example.comgaab.santtos9@gmail.com>
2025-03-24 16:18:10 -0300

    primeiro commit

---
+++ projetos/script.js
@@ -0,0 +1,6 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+// nesse comando, toda vez que o botão for clicado (onclick no html), ele vai procurar no JS essa função do toggleMode 
+  }
